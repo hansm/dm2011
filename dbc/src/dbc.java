2956a0ca70bfaea2ece96afecfa12eebbe3253ea
Sven Aller <allersven@gmail.com>
2011-12-07 17:37:52 +0200

    Added argument textboxes to SNN, bigger window

--- dbc/src/dbc.java
+++ dbc/src/dbc.java
@@ -42,10 +42,14 @@ public class dbc extends Canvas
 	public static ArrayList<DataPoint> points = new ArrayList<DataPoint>();
 	public static HashSet clusters = new HashSet();
 	
-	public static Color[] colors = new Color[24];
+	public static Color[] colors = new Color[25];
 	
 	
 	public static int dotSize = 8;
+	private static JTextField snnK;
+	private static JTextField snnCore;
+	private static JTextField snnNoise;
+	private static JTextField snnLink;
 	
     public dbc()
     {
@@ -64,7 +68,7 @@ public class dbc extends Canvas
     
     public static void clustering3() {
     	kontroll.setText("SNN algorithm: running");
-    	ClusteringAlgorithm algorithm = new SNN(points, 5, 3, 3, 3);
+    	ClusteringAlgorithm algorithm = new SNN(points, Integer.parseInt(snnK.getText()), Integer.parseInt(snnCore.getText()), Integer.parseInt(snnNoise.getText()), Integer.parseInt(snnLink.getText()));
     	try {
     		int clusters = algorithm.run();
     		kontroll.setText("SNN algorithm: finished");
@@ -107,14 +111,15 @@ public class dbc extends Canvas
     }
    
     public static void createColors() {
+    	colors[0] = Color.gray;
     	int[] c = {255, 192, 160, 128};
     	for (int i = 0; i < c.length; i++) {
-    		colors[(0 + 6*i)] = new Color(0, 0, c[i]);
-    		colors[(1 + 6*i)] = new Color(0, c[i], 0);
-    		colors[(2 + 6*i)] = new Color(c[i], 0, 0);
-    		colors[(3 + 6*i)] = new Color(0, c[i], c[i]);
-    		colors[(4 + 6*i)] = new Color(c[i], 0, c[i]);
-    		colors[(5 + 6*i)] = new Color(c[i], c[i], 0);
+    		colors[(1 + 6*i)] = new Color(0, 0, c[i]);
+    		colors[(2 + 6*i)] = new Color(0, c[i], 0);
+    		colors[(3 + 6*i)] = new Color(c[i], 0, 0);
+    		colors[(4 + 6*i)] = new Color(0, c[i], c[i]);
+    		colors[(5 + 6*i)] = new Color(c[i], 0, c[i]);
+    		colors[(6 + 6*i)] = new Color(c[i], c[i], 0);
     	}
     }
     
@@ -191,12 +196,12 @@ public class dbc extends Canvas
         	}
         	
         });
-        canvas.setBounds(275, 40, 500, 500);
+        canvas.setBounds(399, 41, 500, 500);
         JFrame frmDensitybasedClustering = new JFrame();
         frmDensitybasedClustering.setIconImage(Toolkit.getDefaultToolkit().getImage(dbc.class.getResource("/images/icon-logo.gif")));
         frmDensitybasedClustering.setTitle("Density-based clustering");
         frmDensitybasedClustering.setResizable(false);
-        frmDensitybasedClustering.setSize(800, 600);
+        frmDensitybasedClustering.setSize(920, 600);
         frmDensitybasedClustering.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frmDensitybasedClustering.getContentPane().setLayout(null);
         frmDensitybasedClustering.getContentPane().add(canvas);
@@ -206,7 +211,141 @@ public class dbc extends Canvas
         frmDensitybasedClustering.getContentPane().add(kontroll);
         kontroll.setColumns(10);
         
+        JPanel panelStat = new JPanel();
+        panelStat.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+        panelStat.setBounds(175, 552, 227, 75);
+        frmDensitybasedClustering.getContentPane().add(panelStat);
+        panelStat.setLayout(null);
+        
+        JPanel panelCanvas = new JPanel();
+        panelCanvas.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+        panelCanvas.setBounds(394, 36, 510, 510);
+        frmDensitybasedClustering.getContentPane().add(panelCanvas);
+        panelCanvas.setLayout(null);
+        
+        JPanel panelInput = new JPanel();
+        panelInput.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+        panelInput.setBounds(24, 181, 360, 44);
+        frmDensitybasedClustering.getContentPane().add(panelInput);
+        panelInput.setLayout(null);
+        
+        txtFilename = new JTextField();
+        txtFilename.setBounds(10, 11, 298, 20);
+        panelInput.add(txtFilename);
+        txtFilename.setColumns(10);
+        
+        JButton btnOpenFile = new JButton("");
+        btnOpenFile.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-open.gif")));
+        btnOpenFile.setBounds(318, 11, 32, 23);
+        panelInput.add(btnOpenFile);
+        
+        JPanel panelCluster = new JPanel();
+        panelCluster.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+        panelCluster.setBounds(24, 236, 360, 276);
+        frmDensitybasedClustering.getContentPane().add(panelCluster);
+        panelCluster.setLayout(null);
+        
+        JButton btnTestClustering = new JButton("ClusterTest");
+        btnTestClustering.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-cluster.gif")));
+        btnTestClustering.setHorizontalAlignment(SwingConstants.LEFT);
+        btnTestClustering.setBounds(10, 11, 165, 23);
+        panelCluster.add(btnTestClustering);
+        
+        JButton btnClustering1 = new JButton("Clustering 1");
+        btnClustering1.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-cluster.gif")));
+        btnClustering1.setHorizontalAlignment(SwingConstants.LEFT);
+        btnClustering1.setBounds(10, 45, 165, 23);
+        panelCluster.add(btnClustering1);
+        
+        JButton btnClustering2 = new JButton("Clustering 2");
+        btnClustering2.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-cluster.gif")));
+        btnClustering2.setHorizontalAlignment(SwingConstants.LEFT);
+        btnClustering2.setBounds(12, 79, 163, 23);
+        panelCluster.add(btnClustering2);
+        
+        JButton btnClustering3 = new JButton("SNN");
+        btnClustering3.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-cluster.gif")));
+        btnClustering3.setHorizontalAlignment(SwingConstants.LEFT);
+        btnClustering3.setBounds(12, 113, 163, 23);
+        panelCluster.add(btnClustering3);
+        
+        snnK = new JTextField();
+        snnK.setText("5");
+        snnK.setBounds(101, 141, 42, 20);
+        panelCluster.add(snnK);
+        snnK.setColumns(10);
+        
+        JLabel lblsnnK = new JLabel("K:");
+        lblsnnK.setBounds(12, 147, 81, 14);
+        panelCluster.add(lblsnnK);
+        
+        JLabel lblsnnCore = new JLabel("Core threshold:");
+        lblsnnCore.setBounds(10, 172, 81, 14);
+        panelCluster.add(lblsnnCore);
+        
+        snnCore = new JTextField();
+        snnCore.setText("3");
+        snnCore.setColumns(10);
+        snnCore.setBounds(101, 169, 42, 20);
+        panelCluster.add(snnCore);
+        
+        JLabel lblsnnNoise = new JLabel("Noise threshold:");
+        lblsnnNoise.setBounds(169, 145, 81, 14);
+        panelCluster.add(lblsnnNoise);
+        
+        JLabel lblsnnLink = new JLabel("Link threshold:");
+        lblsnnLink.setBounds(169, 170, 81, 14);
+        panelCluster.add(lblsnnLink);
+        
+        snnNoise = new JTextField();
+        snnNoise.setText("3");
+        snnNoise.setColumns(10);
+        snnNoise.setBounds(260, 141, 42, 20);
+        panelCluster.add(snnNoise);
+        
+        snnLink = new JTextField();
+        snnLink.setText("3");
+        snnLink.setColumns(10);
+        snnLink.setBounds(260, 167, 42, 20);
+        panelCluster.add(snnLink);
+        
+        JLabel lblAuthors = new JLabel("Authors: Martin Loginov, Hans M\u00E4esalu, Sven Aller");
+        lblAuthors.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblAuthors.setBounds(539, 553, 363, 14);
+        frmDensitybasedClustering.getContentPane().add(lblAuthors);
+        
+        JLabel lblHeader = new JLabel("");
+        lblHeader.setIcon(new ImageIcon(dbc.class.getResource("/images/header.gif")));
+        lblHeader.setBounds(78, 13, 250, 140);
+        frmDensitybasedClustering.getContentPane().add(lblHeader);
+        
+        JLabel lblElementsLabel = new JLabel("Elements: ");
+        lblElementsLabel.setBounds(464, 13, 62, 14);
+        frmDensitybasedClustering.getContentPane().add(lblElementsLabel);
+        
+        lblElements = new JLabel("0");
+        lblElements.setBounds(525, 13, 46, 14);
+        frmDensitybasedClustering.getContentPane().add(lblElements);
+        
+        lblClusters = new JLabel("");
+        lblClusters.setBounds(651, 13, 46, 14);
+        frmDensitybasedClustering.getContentPane().add(lblClusters);
+        
+        JLabel lblClustersLabel = new JLabel("Clusters:");
+        lblClustersLabel.setBounds(593, 13, 62, 14);
+        frmDensitybasedClustering.getContentPane().add(lblClustersLabel);
+        
+        JLabel lblShowLabel = new JLabel("Show cluster");
+        lblShowLabel.setBounds(707, 13, 94, 14);
+        frmDensitybasedClustering.getContentPane().add(lblShowLabel);
+        
+        choiceClusters = new Choice();
+        choiceClusters.setBounds(803, 10, 101, 20);
+        frmDensitybasedClustering.getContentPane().add(choiceClusters);
+        
         JButton btnClear = new JButton("Clear");
+        btnClear.setBounds(24, 523, 360, 23);
+        frmDensitybasedClustering.getContentPane().add(btnClear);
         btnClear.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-delete.gif")));
         btnClear.addMouseListener(new MouseAdapter() {
         	@Override
@@ -214,101 +353,6 @@ public class dbc extends Canvas
         		clearCanvas();
         	}
         });
-        btnClear.setBounds(24, 428, 227, 23);
-        frmDensitybasedClustering.getContentPane().add(btnClear);
-        
-        JPanel panelStat = new JPanel();
-        panelStat.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-        panelStat.setBounds(24, 462, 227, 75);
-        frmDensitybasedClustering.getContentPane().add(panelStat);
-        panelStat.setLayout(null);
-        
-        JPanel panelCanvas = new JPanel();
-        panelCanvas.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-        panelCanvas.setBounds(270, 35, 510, 510);
-        frmDensitybasedClustering.getContentPane().add(panelCanvas);
-        panelCanvas.setLayout(null);
-        
-        JPanel panelInput = new JPanel();
-        panelInput.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-        panelInput.setBounds(24, 181, 227, 75);
-        frmDensitybasedClustering.getContentPane().add(panelInput);
-        panelInput.setLayout(null);
-        
-        txtFilename = new JTextField();
-        txtFilename.setBounds(10, 11, 207, 20);
-        panelInput.add(txtFilename);
-        txtFilename.setColumns(10);
-        
-        JButton btnOpenFile = new JButton("Open CSV File");
-        btnOpenFile.setHorizontalAlignment(SwingConstants.LEFT);
-        btnOpenFile.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-open.gif")));
-        btnOpenFile.setBounds(10, 40, 207, 23);
-        panelInput.add(btnOpenFile);
-        
-        JPanel panelCluster = new JPanel();
-        panelCluster.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-        panelCluster.setBounds(24, 267, 227, 150);
-        frmDensitybasedClustering.getContentPane().add(panelCluster);
-        panelCluster.setLayout(null);
-        
-        JButton btnTestClustering = new JButton("ClusterTest");
-        btnTestClustering.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-cluster.gif")));
-        btnTestClustering.setHorizontalAlignment(SwingConstants.LEFT);
-        btnTestClustering.setBounds(10, 11, 207, 23);
-        panelCluster.add(btnTestClustering);
-        
-        JButton btnClustering1 = new JButton("Clustering 1");
-        btnClustering1.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-cluster.gif")));
-        btnClustering1.setHorizontalAlignment(SwingConstants.LEFT);
-        btnClustering1.setBounds(10, 45, 207, 23);
-        panelCluster.add(btnClustering1);
-        
-        JButton btnClustering2 = new JButton("Clustering 2");
-        btnClustering2.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-cluster.gif")));
-        btnClustering2.setHorizontalAlignment(SwingConstants.LEFT);
-        btnClustering2.setBounds(12, 79, 205, 23);
-        panelCluster.add(btnClustering2);
-        
-        JButton btnClustering3 = new JButton("SNN");
-        btnClustering3.setIcon(new ImageIcon(dbc.class.getResource("/images/icon-cluster.gif")));
-        btnClustering3.setHorizontalAlignment(SwingConstants.LEFT);
-        btnClustering3.setBounds(12, 113, 205, 23);
-        panelCluster.add(btnClustering3);
-        
-        JLabel lblAuthors = new JLabel("Authors: Martin Loginov, Hans M\u00E4esalu, Sven Aller");
-        lblAuthors.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblAuthors.setBounds(415, 552, 363, 14);
-        frmDensitybasedClustering.getContentPane().add(lblAuthors);
-        
-        JLabel lblHeader = new JLabel("");
-        lblHeader.setIcon(new ImageIcon(dbc.class.getResource("/images/header.gif")));
-        lblHeader.setBounds(10, 25, 250, 140);
-        frmDensitybasedClustering.getContentPane().add(lblHeader);
-        
-        JLabel lblElementsLabel = new JLabel("Elements: ");
-        lblElementsLabel.setBounds(339, 11, 62, 14);
-        frmDensitybasedClustering.getContentPane().add(lblElementsLabel);
-        
-        lblElements = new JLabel("0");
-        lblElements.setBounds(400, 11, 46, 14);
-        frmDensitybasedClustering.getContentPane().add(lblElements);
-        
-        lblClusters = new JLabel("");
-        lblClusters.setBounds(526, 11, 46, 14);
-        frmDensitybasedClustering.getContentPane().add(lblClusters);
-        
-        JLabel lblClustersLabel = new JLabel("Clusters:");
-        lblClustersLabel.setBounds(468, 11, 62, 14);
-        frmDensitybasedClustering.getContentPane().add(lblClustersLabel);
-        
-        JLabel lblShowLabel = new JLabel("Show cluster");
-        lblShowLabel.setBounds(582, 11, 94, 14);
-        frmDensitybasedClustering.getContentPane().add(lblShowLabel);
-        
-        choiceClusters = new Choice();
-        choiceClusters.setBounds(678, 8, 101, 20);
-        frmDensitybasedClustering.getContentPane().add(choiceClusters);
         choiceClusters.addItemListener(new ItemListener() {
         	public void itemStateChanged(ItemEvent arg0) {
 //        		System.out.println(choiceClusters.getSelectedIndex());
